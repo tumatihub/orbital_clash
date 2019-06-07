c9ca94847595c70aaf751e6485cade7312c40491
tumatihub <jgsoaresdias@gmail.com>
2019-06-06 23:25:30 -0300

    Menu principal basico e cenas para jogar single e multiplayer.

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void LoadSinglePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMultiPlayer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
